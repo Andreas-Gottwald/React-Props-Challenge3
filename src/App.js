25e55684b74fecce03eaa603f03affefa4eb3a87
Andreas Gottwald <andreas.gottwald@hotmail.de>
2023-12-27 13:57:19 +0100

    finish

--- src/App.js
+++ src/App.js
@@ -2,5 +2,33 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <>
+      <Smiley isHappy={true} />
+      <Smiley isHappy={false} />
+    </>
+  );
 }
+
+function Smiley({ isHappy }) {
+  return isHappy === true ? <h1>😄</h1> : <h1>🤨</h1>;
+}
+
+/* function Smiley({ isHappy }) {
+  let moodSmiley = null;
+  if (isHappy) {
+    moodSmiley = (
+      <span role="img" aria-label=" A emoji that indicates happyness">
+        🙂
+      </span>
+    );
+  } else {
+    moodSmiley = (
+      <span role="img" aria-label=" A emoji that indicates sadness">
+        ☹️
+      </span>
+    );
+  }
+
+  return <h1>{moodSmiley}</h1>;
+} */
